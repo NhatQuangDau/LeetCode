10571bbd4f8b85274e40fef70e3875ef925c7cc8
Lucius - Nhat Quang Dau <nhatquangdau@gmail.com>
2021-01-17 19:17:57 +0700

    Refactor 6

--- word-search.py
+++ word-search.py
@@ -1,8 +1,10 @@
+moves = [[1,0],[-1,0],[0,1],[0,-1]]
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         startSearch = word[0]
         startPoints = []
-        moves = [[1,0],[-1,0],[0,1],[0,-1]]
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == startSearch:
@@ -10,11 +12,13 @@ class Solution:
         print(startPoints)
         checkBoard = [[False]*len(board[0]) for _ in range(len(board))]
         print(checkBoard)
-        for i in range(len(startPoints)):
-            if self.backtrack(board, checkBoard, startPoints[i][0], startPoints[i][1], 0, word, moves):
+        for point in startPoints:
+            if self.backtrack(board, checkBoard, point[0], point[1], 0, word):
                 return True
+        
+        return False
             
-    def backtrack(self, board, checkBoard, i, j, strLength, word, moves):
+    def backtrack(self, board, checkBoard, i, j, strLength, word):
         if strLength == len(word):
             return True
         if board[i][j] != word[strLength]:
@@ -25,14 +29,12 @@ class Solution:
         tempResult = False
         for h in range(len(moves)):
             if self.canMove(i, j, moves[h], checkBoard):
-                tempResult = tempResult or self.backtrack(board, checkBoard, i + moves[h][0], j + moves[h][1], strLength + 1, word)
-        if tempResult:        
-            return tempResult
+                tempResult = tempResult or self.backtrack(board, checkBoard, i + moves[h][0], j + moves[h][1], strLength + 1, word)    
         checkBoard[i][j] = False
-        return False
+        return tempResult
         
-    def canMove(self, i, j, moves, checkBoard):
-        if i + moves[0] >= len(checkBoard) or i + moves[0] < 0 or j + moves[1] >= len(checkBoard[0]) or j + moves[1] < 0:
+    def canMove(self, i, j, move, checkBoard):
+        if i + move[0] >= len(checkBoard) or i + move[0] < 0 or j + move[1] >= len(checkBoard[0]) or j + move[1] < 0:
             return False
         if checkBoard[i][j] == True:
             return False
