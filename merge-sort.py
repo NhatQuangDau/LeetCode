513f956873ffa538f63205d52f18a87a470401b6
NhatQuangDau <nhatquang9129@yahoo.com>
2021-02-22 03:35:25 +0700

    Update merge-sort.py
    2nd commit, faster than 49%, mem consumes less than 88%

--- merge-sort.py
+++ merge-sort.py
@@ -7,14 +7,13 @@ class Solution:
         mid = len(arr)//2
         if mid == 0:
             return arr
-        return self.merge(self.mergeSort(arr[:mid]), self.mergeSort(arr[mid:]))
+        return self.merge(arr, self.mergeSort(arr[:mid]), self.mergeSort(arr[mid:]))
     
-    def merge(self, left, right) -> List[int]:
+    def merge(self,arr, left, right) -> List[int]:
         i = 0 #index left arr
         j = 0 #index right arr
         l = len(left)
         r = len(right)
-        arr = [0]*(l+r)
         k = 0 #index of mergeArr
         while i < l and j < r:
             if left[i] < right[j]:
