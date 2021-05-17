f67759d02892087af2bd87ce6904ee48f9652fe7
NhatQuangDau <nhatquang9129@yahoo.com>
2021-05-17 14:09:41 +0700

    2nd commit
    this is the correct solution, base on recognizing the pattern, there is no backtracking involved. go to https://www.youtube.com/watch?v=W9SIlE2jhBQ&t=514s for best explaination

--- solution.py
+++ solution.py
@@ -1,25 +1,19 @@
-#Simple implementation - generate all permutation possible until permutation kth, then stop, this can solve the problem if input test cases one by one, but when run all test cases, it will be TLE
+#Correct solution - for more info goto https://www.youtube.com/watch?v=W9SIlE2jhBQ&t=514s
+import math
 class Solution:
-    dictPer = dict()
-    pos = 0
     def getPermutation(self, n: int, k: int) -> str:
         arr = []
+        result = ""
         for i in range(1, n+1):
             arr.append(i)
-        visited = [False] * n
-        self.generatePer(visited, arr, '', k)
-        return self.dictPer[k-1]
-    def generatePer(self, visited, arr, temp, k):
-        if pos == k:
-            return
-        if len(temp) == len(arr):
-            self.dictPer[self.pos] = temp
-            self.pos += 1
-            return
-        for i in range(len(arr)):
-            if visited[i] == False:
-                visited[i] = True
-                temp += str(arr[i])
-                self.generatePer(visited, arr, temp, k)
-                visited[i] = False
-                temp = temp[:-1]
+        print(arr)
+        k -= 1
+        pos = 1 # position from which it make factorial(n - pos) > k
+        while k != 0:
+            candidateIndex = k//math.factorial(n-pos)
+            result += str(arr.pop(candidateIndex))
+            k = k%math.factorial(n-pos)
+            pos += 1
+        result += "".join(map(str, arr))
+        print(arr)
+        return result
