b025d6213af251681e20573fb71797973f686d74
NhatQuangDau <nhatquang9129@yahoo.com>
2021-06-12 17:03:05 +0700

    2nd commit
    improve on the algorithm, use n^2 only, use 3 board of checker: row check, column check, box check, but it will use more space.

--- solution.py
+++ solution.py
@@ -2,22 +2,16 @@
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowCheck = [[0 for i in range(10)] for j in range(9)]
+        columnCheck = [[0 for i in range(10)] for j in range(9)]
+        boxCheck = [[0 for i in range(10)] for j in range(9)]
         for row in range(9):
             for column in range(9):
-                if board[row][column] == '.':
-                    continue
-                if not self.validate(board, row, column):
-                    return False
-        return True
-    
-    def validate(self, board, row, column) -> bool:
-        for i in range(row + 1, 9):
-            if board[row][column] == board[i][column]:
-                return False
-        for i in range(column + 1, 9):
-            if board[row][column] == board[row][i]:
-                return False
-        for i in range((row % 3) * 3 + column % 3 + 1, 9):
-            if board[row][column] == board[row//3 * 3 + i//3][column//3 * 3 + i % 3]:
-                return False
+                if board[row][column] != '.':
+                    num = int(board[row][column])
+                    if rowCheck[row][num] == 1 or columnCheck[column][num] == 1 or boxCheck[row//3 * 3 + column//3][num] == 1:
+                        return False
+                    rowCheck[row][num] = 1
+                    columnCheck[column][num] = 1
+                    boxCheck[row//3 * 3 + column//3][num] = 1
         return True
